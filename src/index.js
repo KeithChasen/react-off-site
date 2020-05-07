7fc1dc8f718a14b8a74e984939b496217e5d3a16
keithchasen <keithchasen89@gmail.com>
2020-05-07 22:04:54 +0300

    used api context

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ApiContext from './context/api'
+import Api from "./api/api";
+
+const api = new Api();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ApiContext.Provider value={ api }>
+          <App />
+      </ApiContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
