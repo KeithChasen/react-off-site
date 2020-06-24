3c527ae9358c1fde83bef4bbd2e7dbda1553aa1d
keithchasen <keithchasen89@gmail.com>
2020-06-24 17:53:38 +0300

    sign in using firebase

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,18 +1,23 @@
 import React, { useRef } from 'react';
-import { signUp } from '../../service/auth/auth';
+import { signUp, signIn } from '../../service/auth/auth';
 
 const Auth = () => {
 
   const signUpSubmitted = event => {
     event.preventDefault();
-
-    console.log(`Email: ${email.current.value}, Password: ${password.current.value}`)
-
     signUp(email.current.value, password.current.value);
+  };
+
+  const signInSubmitted = event => {
+    event.preventDefault();
+    signIn(signInEmail.current.value, signInPassword.current.value);
   };
 
   const email = useRef(null);
   const password = useRef(null);
+
+  const signInEmail = useRef(null);
+  const signInPassword = useRef(null);
 
   return (
     <div>
@@ -23,7 +28,13 @@ const Auth = () => {
       <form onSubmit={signUpSubmitted}>
         <input type="email" ref={email} placeholder='email'/>
         <input type="password" ref={password} placeholder='password'/>
-        <input type="submit"/>
+        <input type="submit" value='Sign Up'/>
+      </form>
+
+      <form onSubmit={signInSubmitted}>
+        <input type="email" ref={signInEmail} placeholder='email'/>
+        <input type="password" ref={signInPassword} placeholder='password'/>
+        <input type="submit" value='Sign In'/>
       </form>
     </div>
   );
--- src/service/auth/auth.js
+++ src/service/auth/auth.js
@@ -5,5 +5,10 @@ firebase.initializeApp(firebaseConfig);
 
 export const signUp = (email, password) =>
   firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(response => console.log(`Sign Up Response: ${response}`))
-    .catch(error => console.error(`Sign Up Error: ${error}`));
+    .then(response => console.log('Sign Up Response: ', response))
+    .catch(error => console.error('Sign Up Error: ', error));
+
+export const signIn = (email, password) =>
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(response => console.log('Sign In Response: ', response))
+    .catch(error => console.log('Sign In Error: ', error));
