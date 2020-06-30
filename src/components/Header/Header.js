e9639542a216a4142caddf852612a80fcee725cc
keithchasen <keithchasen89@gmail.com>
2020-07-01 00:25:57 +0300

    different header links for authenticated and non-authenticated user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import {NavLink, Link, useHistory} from "react-router-dom";
+import React, {useState} from 'react';
+import { NavLink, Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
-import { signOut } from '../../service/auth/auth';
-import { userLogOut } from "../../store/actions";
+import { checkAuth, signOut } from '../../service/auth/auth';
+import { userLoaded, userLogOut } from "../../store/actions";
 import { useDispatch } from "react-redux";
 
 const HeaderList = styled.ul`
   list-style-type: none;
-  margin: 0;
+  margin: 0; 
   padding: 0;
   overflow: hidden;
   background-color: #333;
@@ -25,10 +25,21 @@ const HeaderListItem = styled.li`
   }
 `;
 
-
 const Header = () => {
   const dispatch = useDispatch();
   let history = useHistory();
+  const [ loader, setLoader ] = useState(true);
+  const [ isAuth, setIsAuth ] = useState(false);
+
+  checkAuth(user => {
+    if (user) {
+      dispatch(userLoaded(user));
+      setIsAuth(true);
+    } else {
+      setIsAuth(false);
+    }
+    setLoader(false);
+  });
 
   const logOut = () => {
     signOut()
@@ -39,22 +50,29 @@ const Header = () => {
       .catch(error => console.log('Sign out Error: ', error))
   };
 
+  const headerLinks = isAuth ?
+    <HeaderList>
+      <HeaderListItem>
+        <NavLink exact to='/'>Home</NavLink>
+      </HeaderListItem>
+      <HeaderListItem>
+        <Link to='#' onClick={ logOut }>Logout</Link>
+      </HeaderListItem>
+    </HeaderList> :
+    <HeaderList>
+      <HeaderListItem>
+        <NavLink to='/register'>Register</NavLink>
+      </HeaderListItem>
+      <HeaderListItem>
+        <NavLink to='/login'>Login</NavLink>
+      </HeaderListItem>
+    </HeaderList>;
+
+  const content = loader ? loader : headerLinks;
+
   return (
     <div>
-      <HeaderList>
-        <HeaderListItem>
-          <NavLink exact to='/'>Home</NavLink>
-        </HeaderListItem>
-        <HeaderListItem>
-          <NavLink to='/register'>Register</NavLink>
-        </HeaderListItem>
-        <HeaderListItem>
-          <NavLink to='/login'>Login</NavLink>
-        </HeaderListItem>
-        <HeaderListItem>
-          <Link to='#' onClick={ logOut }>Logout</Link>
-        </HeaderListItem>
-      </HeaderList>
+      { content }
     </div>
   );
 };
